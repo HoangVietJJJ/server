e7fb8a1b5281efc8010c4ddf57de30d48e7d7736
HoangViet <hoangphamhl000@gmail.com>
2024-06-02 00:13:03 +0700

    Coding pagination

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -11,3 +11,16 @@ export const getPosts = async (req, res) => {
         })
     }
 }
+
+export const getPostsLimitController = async (req, res) => {
+    const { page } = req.query
+    try {
+        const response = await postService.getPostsLimitService(page)
+        return res.status(200).json(response)
+    } catch (e) {
+        res.status(500).json({
+            err: -1,
+            msg: 'Fail at post controller' + e
+        })
+    }
+}
--- src/services/post.js
+++ src/services/post.js
@@ -1,11 +1,36 @@
 import { raw } from 'express'
 import db from '../models'
+require('dotenv').config()
 
 export const getPostsService = () => new Promise(async (resolve, reject) => {
     try {
         const response = await db.Post.findAll({
             raw: true,
             nest: true,
+            include: [
+                { model: db.Image, as: 'images', attributes: ['image'] },
+                { model: db.Attribute, as: 'attributes', attributes: ['price', 'acreage', 'published', 'hashtag'] },
+                { model: db.User, as: 'user', attributes: ['name', 'zalo', 'phone'] }
+            ],
+            attributes: ['id', 'title', 'star', 'address', 'description'],
+        })
+        resolve({
+            err: response ? 0 : 1,
+            msg: response ? 'OK' : 'Failed to get Posts',
+            response
+        })
+    } catch (e) {
+        reject(e)
+    }
+})
+
+export const getPostsLimitService = (offset) => new Promise(async (resolve, reject) => {
+    try {
+        const response = await db.Post.findAndCountAll({
+            raw: true,
+            nest: true,
+            offset: offset * (+process.env.LIMIT) || 0,
+            limit: +process.env.LIMIT,
             include: [
                 { model: db.Image, as: 'images', attributes: ['image'] },
                 { model: db.Attribute, as: 'attributes', attributes: ['price', 'acreage', 'published', 'hashtag'] },
